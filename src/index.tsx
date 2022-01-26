756477ebee748aafe95f24e6de06ac9f181b922f
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-26 19:35:56 +0300

    lesson3 promise trainer1

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/state';
+import Lesson3 from "./lessons/lesson3/Lesson3";
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Lesson3 />
     </Provider>,
     document.getElementById('root')
 );
--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -10,7 +10,25 @@ console.log('lesson 3');
 // Promise
 // https://learn.javascript.ru/promise-basics
 // https://www.youtube.com/watch?v=1idOY3C1gYU
+ let prom = new Promise((resolve,reject)=>{
+    setTimeout((response)=>{
+        if (response.status>=200 && response.status<400 ) {
+            resolve(response.body)
+        }
+        else {
+            reject(response.error)
+        }
+    },200,{status:200,body:{data:'success'},error:''})
+})
 
+
+prom.then((response)=>{
+    console.log(response)
+})
+console.log(prom)
+// Promise{<pending>}
+//undefined
+//{data: 'success'}
 
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
